29ad3b54480664eabb1e39758db54a733334660d
Alex <36587433+thedeadweather@users.noreply.github.com>
2020-03-24 13:53:28 +0300

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -1,29 +1,29 @@
 class Game
   TOTAL_ERRORS_ALLOWED = 7
 
-# конструктор с переменными:
-# слово по буквам и массив попыток
+  # конструктор с переменными:
+  # слово по буквам и массив попыток
   def initialize(word)
     @letters = word.chars
     @user_guesses = []
   end
 
-# массив попыток - массив загаданных букв
+  # массив попыток - массив загаданных букв
   def errors
     return @user_guesses - normalized_letters
   end
 
-# длина массива ошибок
+  # длина массива ошибок
   def errors_made
     return errors.length
   end
 
-# 7 - кол-во ошибок
+  # 7 - кол-во ошибок
   def errors_allowed
     return TOTAL_ERRORS_ALLOWED - errors_made
   end
 
-# вернем букву если она есть в попытках либо nil
+  # вернем букву если она есть в попытках либо nil
   def letters_to_guess
     result =
       @letters.map do |letter|
@@ -37,7 +37,7 @@ class Game
     return result
   end
 
-# вернуть измененную букву "ё" на "е" и "й" на "и"
+  # вернуть измененную букву "ё" на "е" и "й" на "и"
   def normalize_letter(letter)
     case letter
     when "Ё"
@@ -49,18 +49,17 @@ class Game
     end
   end
 
-# вернуть массив с измененными буквами
+  # вернуть массив с измененными буквами
   def normalized_letters
     @letters.map { |i| normalize_letter(i) }
   end
 
-# сыграть: добавить букву в попытки
+  # сыграть: добавить букву в попытки
   def play!(letter)
     if !over? && !@user_guesses.include?(normalize_letter(letter))
       @user_guesses << normalize_letter(letter)
     end
   end
-
 
   def won?
     return (normalized_letters - @user_guesses).empty?
@@ -74,7 +73,7 @@ class Game
     return won? || lost?
   end
 
-# вернуть слово
+  # вернуть слово
   def word
     return @letters.join
   end
